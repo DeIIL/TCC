3de4eea1b66ef04b22bd6cf13aa5f3205b58e873
Gustavo Del Castillo <gudellcastillo@gmail.com>
2021-11-19 19:29:03 -0300

    resolving errors in the colors file

--- app/src/main/java/com/example/tcc/CreateAccount2nd.java
+++ app/src/main/java/com/example/tcc/CreateAccount2nd.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 public class CreateAccount2nd extends AppCompatActivity {
 
     ImageButton btn_arrow;
+    android.widget.Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +19,21 @@ public class CreateAccount2nd extends AppCompatActivity {
         setContentView(R.layout.activity_create_account2nd);
 
         btn_arrow = findViewById(R.id.btn_arrow);
+        btn_next = findViewById(R.id.btn_next_create_account);
 
         btn_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(CreateAccount2nd.this, CreateAccount.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(CreateAccount2nd.this, HomeActivity.class);
+                startActivity(in);
             }
         });
     }
